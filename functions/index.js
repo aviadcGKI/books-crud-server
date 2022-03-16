3bdcfa33cd9748aa9bca7d00102a37a5a0e0c19f
aviadc <coaviadco@gmail.com>
2022-03-16 11:49:47 +0200

    restructure the functions folder

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,11 @@
-const functions = require("firebase-functions");
-const cors = require('cors')({origin: true});
 const admin = require('firebase-admin');
 
 admin.initializeApp();
 const db = admin.firestore();
+module.exports = db;
 
-// const app = express();
+module.exports = {
+    ...require('./src/controllers/books.controller.js'),
+    ...require('./src/controllers/authors.controller.js'),
+};
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const booksRef = db.collection("books");
-
-exports.getBooksByPages = functions.https.onRequest(async (req, res) => {
-    cors(req,res, async ()=>{
-        try{
-            const booksData = await booksRef.where("pages","<=",req.body.pages).get();
-            const activeBooksList = [];
-            booksData.docs.forEach((book) => {
-              activeBooksList.push({ ...book.data(), id: book.id })
-            })
-            res.status(200).send(activeBooksList);
-        }catch(err){
-            res.status(400).send(err);
-        }
-        
-    })
-})
-
-// app.get('/books-pages', (req, res) => {
-//     this.getBooksByPages(req, res);
-// })
